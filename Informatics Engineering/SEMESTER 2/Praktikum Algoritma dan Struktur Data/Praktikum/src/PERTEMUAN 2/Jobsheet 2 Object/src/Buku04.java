4a4310138b17cf985331de54dda297060f634adb
afadlih <144323171+afadlih@users.noreply.github.com>
2024-02-20 08:17:42 +0700

    P2_Percobaan 1 _ 04 _ PASD

---
+++ Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 2/Jobsheet 2 Object/src/Buku04.java
@@ -0,0 +1,23 @@
+public class Buku04 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilinformasi() {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah Halaman : " + halaman);
+        System.out.println("Sisa Stok : " + stok);
+        System.out.println("Harga : Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+    void restok(int jml) {
+        stok += jml;
+    }
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+}
