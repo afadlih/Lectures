480773ff32518d258568a87dade1e20e1e95a3c6
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-02-29 14:53:09 +0700

    P3 _ Percobaan 3 _ Pertanyaan 5 _ 04 _ PASD

--- Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 3/ArrayObject/src/ArraySegitiga/main.java
+++ Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 3/ArrayObject/src/ArraySegitiga/main.java
@@ -9,7 +9,12 @@ public class main {
         sgArray[2] = new Segitiga(15, 6);
         sgArray[3] = new Segitiga(25, 10);
         
-        
+        for (int i = 0; i < sgArray.length; i++) {
+            System.out.println("Segitiga ke-" + (i+1));
+            System.out.println("Luas: " + sgArray[i].hitungLuas());
+            System.out.println("Keliling: " + sgArray[i].hitungKeliling());
+            System.out.println();
+        }
     }
     
 }
