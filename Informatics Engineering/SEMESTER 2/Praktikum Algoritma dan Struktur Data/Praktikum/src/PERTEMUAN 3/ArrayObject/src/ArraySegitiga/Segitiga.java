337649c34b370595e0620511041857a24f9d79f8
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-02-29 14:17:37 +0700

    P3_Percobaan 3 _ 04 _ Pertanyaan 2_ PASD

---
+++ Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 3/ArrayObject/src/ArraySegitiga/Segitiga.java
@@ -0,0 +1,11 @@
+package ArraySegitiga;
+
+public class Segitiga {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+}
