fa6c3541f8b40c5a31fbad64a5998661a73f4b4c
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-02-29 14:51:09 +0700

    P3_Percobaan 3_Pertanyaan 3_04_PASD

--- Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 3/ArrayObject/src/ArraySegitiga/Segitiga.java
+++ Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 3/ArrayObject/src/ArraySegitiga/Segitiga.java
@@ -8,4 +8,13 @@ public class Segitiga {
         alas = a;
         tinggi = t;
     }
+    public double hitungLuas() {
+        return (alas * tinggi) / 2.0;
+    }
+
+    public double hitungKeliling() {
+        double sisiMiring = Math.sqrt((alas * alas) + (tinggi * tinggi));
+        return alas + tinggi + sisiMiring;
+    }
+    
 }
