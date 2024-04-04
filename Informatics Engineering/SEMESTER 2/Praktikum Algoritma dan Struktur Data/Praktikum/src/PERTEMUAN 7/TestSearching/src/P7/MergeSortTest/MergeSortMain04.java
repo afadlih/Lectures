ba1685e152b9299bbc641b9150711a92a49bb58b
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-04-05 00:11:51 +0700

    P7_ Percobaan 3_04_PASD

---
+++ Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 7/TestSearching/src/P7/MergeSortTest/MergeSortMain04.java
@@ -0,0 +1,14 @@
+package P7.MergeSortTest;
+
+public class MergeSortMain04 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("Sorting dengan merge sort");
+        MergeSorting04 mSort = new MergeSorting04();
+        System.out.println("Data Awal");
+        mSort.printArray(data);
+        mSort.MergeSort(data);
+        System.out.println("Setelah diurutkan");
+        mSort.printArray(data);
+    }
+}
