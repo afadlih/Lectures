64352e9a70ac097e2375e20d5f7d0e39d5f74836
afadlih <correct-144323171+afadlih@users.noreply.github.com>
2024-06-09 11:07:56 +0700

    P15_Percobaan 2_04_PASD

--- Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 15/Graph/GraphMain04.java
+++ Informatics Engineering/SEMESTER 2/Praktikum Algoritma dan Struktur Data/Praktikum/src/PERTEMUAN 15/Graph/GraphMain04.java
@@ -21,5 +21,18 @@ public class GraphMain04 {
             gedung.Neighbours(asal, tujuan);
             System.out.println("");
         }
+        System.out.println("================================");
+        GraphMatriks04 gdg = new GraphMatriks04(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2,1);
+        gdg.printGraph();
+        
     }
 }
